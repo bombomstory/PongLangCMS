fdae7281325f6ec1ae9ab754b5f7c387d3542409
Chanachai <chnachaysurinthr2@gmail.com>
2022-02-10 15:08:47 +0700

    add

---
+++ Lab_007.php
@@ -0,0 +1,16 @@
+<?php
+    $test1 = true;
+    $test2 = 1234;
+    $test3 = 4.50;
+    $test4 = "Test Data";
+
+    if($test1 == false){
+        echo "test1 = เท็จ<br>";
+    }else{
+        echo "test1 = จริง<br>";
+    }
+    
+    echo $test2."<br>";
+    echo $test3."<br>";
+    echo $test4."<br>";
+?>
