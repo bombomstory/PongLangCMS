526cd7368208fd4f48f2d898642cce63cff460c2
bombomstory <bombomstory@gmail.com>
2022-02-10 15:00:53 +0700

    Add file Lab_006.php

---
+++ Lab_006.php
@@ -0,0 +1,8 @@
+<?php
+    $name = "My name is Paitoon.";
+    $Name = "My name is Pornsiri.";
+    $namE = "My name is Chanikan";
+    echo $name."<br>";
+    echo $Name."<br>";
+    echo $namE."<br>";
+?>
