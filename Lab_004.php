cd84f5c2fff93577afc06534238a17dddc162cf1
TUSNa <kitsada.ch@ksu.ac.th>
2022-02-10 14:44:35 +0700

    Add

---
+++ Lab_004.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>ทดสอบการแทรกโค้ด PHP ในภาษา HTML</title>
+    </head>
+    <body>
+        <?php
+        echo "<H1>นี่คือข้อความจากภาษา PHP</H1>";
+        ?>
+    </body>
+</html>
