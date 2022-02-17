c73216a26f516bf12e26cb5f256ce63903e75836
piyathida <piyathida1234a@gmail.com>
2022-02-17 15:19:51 +0700

    เพิ่มไฟล์ Lob_016

--- LOb_015.php
+++ LOb_015.php
@@ -1,9 +1,8 @@
 <?php
-    for($i=1; $i<10; $i++){
+    $i = 1;
+    while($i<10){
         echo $i;
         echo "</br>";
-        if($i==5){
-            break;
-        }
+        $i++;
     }
 ?>
