76c290d35d9c0eb40997b3b1380fc85c730b92a5
bombomstory <bombomstory@gmail.com>
2022-02-17 15:53:24 +0700

    แก้ไขฟังก์ชันแบบคืนค่าได้

--- Lab_018.php
+++ Lab_018.php
@@ -1,3 +1,10 @@
 <?php
-    phpinfo();
+    function multiple($x, $y){
+        return $x*$y;
+    }
+
+    $a = 2.71;
+    $b = 3.14;
+    echo "ผลคูณของ $a กับ $b มีค่าเท่ากับ ".multiple($a,$b);
+
 ?>
