b3de20b0bcb6c1686c7b290b7b30d684d2bc68c5
fonjaru <jaruwan.wo@ksu.ac.th>
2022-02-17 15:57:17 +0700

    Add Lab_018

--- Lab_017.php
+++ Lab_017.php
@@ -1,12 +1,9 @@
 <?php
     $book = [
-                'NodeJS'=>'5',
-                'JAVA'=>'12',
-                'PHP'=>'8',
-                'JQuery'=>'3'
+            'NodeJS'=>'5',
+            'JAVA'=>'12',
+            'PHP'=>'8',
+            'JQuery'=>'3'
             ];
     
-    foreach($book as $key=>$value){
-        echo "ชื่อหนังสือ $key จำนวน $value เล่ม<br>";
-    }
 ?>
