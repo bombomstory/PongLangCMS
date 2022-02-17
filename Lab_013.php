c782e1b70104bf9d6e8924f2c1a82077950e8d83
bombomstory <bombomstory@gmail.com>
2022-02-17 14:30:12 +0700

    Add lab 13

---
+++ Lab_013.php
@@ -0,0 +1,44 @@
+<?php
+    $month = 2;
+    switch($month){
+        case 1:
+                echo "เดือนมกราคม";
+                break;
+        case 2:
+               echo "เดือนกุมภาพันธ์";
+                break;  
+        case 3:
+                echo "เดือนมีนาคม";
+                break;      
+        case 4:
+                echo "เดือนเมษายน";
+                break;
+        case 5:
+                echo "เดือนพฤษภาคม";
+                break;  
+        case 6:
+                echo "เดือนมิถุนายน";
+                break;
+        case 7:
+                echo "เดือนกรกฎาคม";
+                break;
+        case 8:
+                echo "เดือนสิงหาคม";
+                break;  
+        case 9:
+                echo "เดือนกันยายน";
+                break;      
+        case 10:
+                echo "เดือนตุลาคม";
+                break;
+        case 11:
+                echo "เดือนพฤศจิกายน";
+                break;  
+        case 12:
+                echo "เดือนธันวาคม";
+                break;   
+        default:
+                echo "หมายเลขเดือน ไม่ถูกต้อง";
+                break;                         
+    }
+?>
